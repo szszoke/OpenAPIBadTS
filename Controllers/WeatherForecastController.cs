cb8f18ecefd659116af81da142a29f17bef5ecda
Szabolcs Szőke <szszoke.code@gmail.com>
2021-05-03 09:40:02 +0200

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenaAPIBadTS.Controllers
+{
+    public class QueryResponse
+    {
+        public int Data { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<IDictionary<string, IEnumerable<QueryResponse>>> Query()
+        {
+            return NoContent();
+        }
+    }
+}
